b07d65f2e06f5717e97773761f932e658ca625eb
Tony Brown <tony.brown.357@gmail.com>
2014-07-05 05:58:52 -0400

    backbone blog starting template

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
+/*jslint nomen: true, sloppy: true, node: true, es5: true */
+/*global require: false */
+
 var express = require('express');
-var path    = require('path');
-var Bourne  = require('bourne');
-// using componets from node require
-var app      = express()
-  , posts    = new Bourne('simpleBlogPosts.json')
-  , comments = new Bourne('simpleBlogComments.json');
-// configuration for the app
+var path = require('path');
+var Bourne = require('bourne');
+
+var app = express();
+var posts = new Bourne('simpleBlogPosts.json');
+var comments = new Bourne('simpleBlogComments.json');
+
 app.configure(function () {
-  app.use(express.json);
+  app.use(express.json());
   app.use(express.static(path.join(__dirname, 'public')));
 });
-// create the route method
+
 app.get('/*', function (req, res) {
-  res.render('index.ejs');        
+  posts.find(function (err, results) {
+    res.render('index.ejs', { posts: JSON.stringify(results)});
+  });
 });
 
+app.listen(3333);
