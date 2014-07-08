56fb11404693bf4656163c1f08f41dc6872eb9cd
Tony Brown <tonybrown@tony-dev.home>
2014-07-08 03:43:09 -0400

    update

--- public/app.js
+++ public/app.js
@@ -13,6 +13,35 @@ var Posts = Backbone.Collection.extend({
   url: '/posts'
 });
 
+var PostFormView = Backbone.View.extend({
+  tagName: 'form',
+  template: _.template($('#postFormView').html()),
+  initialize: function (options) {
+    this.posts = options.posts;
+  },
+
+  events: {
+    'click button': 'createPost'
+  },
+
+  render: function () {
+    this.el.innerHTML = this.template();
+    return this;
+  },
+
+  createPost: function (e) {
+    var postAttrs = {
+      content: $('#postText').val(),
+      title: $('#postTitle').val(),
+      pubDate: new Date()
+    };
+
+    this.posts.create(postAttrs);
+    postRouter.navigate('/', { trigger: true  });
+      return false;
+  }
+});
+
 var PostListView = Backbone.View.extend({
   tagName: 'li',
   template: _.template("<a href='/posts/{{id}}'>{{title}}</a>"),
@@ -38,8 +67,8 @@ var PostsListView = Backbone.View.extend({
     this.el.innerHTML = this.template();
     var ul = this.$el.find('ul');
     this.collection.forEach(function (post) {
-      ul.append(new PostListView({ 
-        model: post 
+      ul.append(new PostListView({
+        model: post
       }).render().el);
     });
     return this;
@@ -82,8 +111,15 @@ var PostRouter = Backbone.Router.extend({
     this.main.html(pv.render().el);
   },
 
+  newPost: function () {
+    var pfv = new PostFormView({ posts: this.posts  });
+      this.main.html(pfv.render().el);
+  },
+
   singlePost: function (id) {
-    console.log('view post '+ id);
+    var post = this.posts.get(id);
+    var pv = new PostView({ model: post });
+    this.main.html(pv.render().el);
   }
 });
 
